3418c94bca7844226870ce22fa3176eed6fc78ac
ConePerez <oscarerik@gmail.com>
2020-10-02 23:09:30 -0500

    Adding latest version of the files

--- plot1.R
+++ plot1.R
@@ -6,19 +6,17 @@
 ## 1.- Downloads a zip file containing the data to be plotted
 ## 2.- Opens a connection to the downloaded file.
 ## 3.- Reads the files into a data frame objet
-## 4.- Close the file connection
-## 5.- Create a subset of the data that is going to be used in the plot
-## 6.- Remove the complete data set to release memory
-## 7.- Set the global parameter to one plot per device.
-## 8.- Create the histogram plot
-## 9.- Copy the historgram plot from the screen device to the PNG device
-## 10.- Close the PNG device
+## 4.- Create a subset of the data that is going to be used in the plot
+## 5.- Remove the complete data set to release memory
+## 6.- Set the global parameter to one plot per device.
+## 7.- Create the histogram plot
+## 8.- Copy the historgram plot from the screen device to the PNG device
+## 9.- Close the PNG device
 
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile="data.zip", method="curl")
 f = unz(description="data.zip",filename="household_power_consumption.txt")
 open(f)
 exp.proj1 = read.delim(f, sep=";", na.string="?")
-close(f)
 x = subset(exp.proj1, (Date == "1/2/2007" | Date == "2/2/2007"))
 remove(exp.proj1)
 par(mfrow=c(1,1))
--- plot4.R
+++ plot4.R
@@ -1,26 +1,27 @@
-## File plot1.R
+## File plot4.R
 ## The file contains code to produce a plot in the screen and then copy it to a PNG file.
 
 ## Usage: The file must be loaded into an R workspace with the run function or copy and paste all the lines in the prompt.
 ## The sequence of the lines below do the following actions:
+
 ## 1.- Downloads a zip file containing the data to be plotted
 ## 2.- Opens a connection to the downloaded file.
 ## 3.- Reads the files into a data frame objet
-## 4.- Close the file connection
-## 5.- Create a subset of the data that is going to be used in the plot
-## 6.- Remove the complete data set to release memory
-## 7.- Set the global parameter to 4 plots per device.
-## 8.- Create the first scatter plot
-## 8.1.- Create the second scatter plot
-## 8.2.- Create the third scatter plot
-## 8.3.- Create the fourth scatter plot
-## 9.- Copy the scatter plot from the screen device to the PNG device
-## 10.- Close the PNG device
+## 4.- Create a subset of the data that is going to be used in the plot
+## 5.- Remove the complete data set to release memory
+## 6.- Set the global parameter to 4 plots per device.
+## 7.- Create the first scatter plot
+## 7.1.- Create the second scatter plot
+## 7.2.- Create the third scatter plot
+## 7.3.- Create the fourth scatter plot
+## 8.- Copy the scatter plot from the screen device to the PNG device
+## 9.- Close the PNG device
 
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile="data.zip", method="curl")
 f = unz(description="data.zip",filename="household_power_consumption.txt")
 exp.proj1 = read.delim(f, sep=";", na.string="?")
-close(f)
+t <- paste(x$Date, x$Time)
+t <- strptime(t, "%d/%m/%Y %H:%M:%S")
 x = subset(exp.proj1, (Date == "1/2/2007" | Date == "2/2/2007"))
 remove(exp.proj1)
 par(mfrow=c(2,2))
